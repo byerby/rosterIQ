d37c73fb09b56337e5e4039e2b7ac59bd2a221ef
Ben Yerby <benyerby1@gmail.com>
2019-05-21 00:37:39 -0400

    Added About Page

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ var geoJsonLayer = L.geoJson(null, {
         return thisYear && thisTeam && thisPosition
     },
     onEachFeature: function (feature, layer) {
-//        if (feature.properties.Position == "QB") {
-//            feature.properties.Position = "Quarterback"
-//        }
+        //        if (feature.properties.Position == "QB") {
+        //            feature.properties.Position = "Quarterback"
+        //        }
 
-        layer.bindPopup("<span class='firstName'>" + feature.properties.FirstName + " </span>" + "<span class='lastName'>" + feature.properties.LastName + "</span>" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<span class='uniformNumber'>" + feature.properties.UniformNumber + "</span>" + "<br />" + "<span class='teamName'>" + feature.properties.TeamName + "</span>" + "<br />" + "<span class='playerPosition'>" + feature.properties.Position + "</span>" + "<br />" + "<span class='playerHeight'>" + feature.properties.Height + "</span>" + " in" + "<br />" + "<span class='playerWeight'>" + feature.properties.Weight + "</span>" + " lbs" + "<br />" + "<span class='lastSchool'>" + feature.properties.LastSchool + "</span>")
+        layer.bindPopup("<span class='firstName'>" + feature.properties.FirstName + " </span>" + "<span class='lastName'>" + feature.properties.LastName + "</span>" + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "<div class='uniformNumber-box'><span class='uniformNumber'>" + feature.properties.UniformNumber + "</span></div>" + "<br />" + "<span class='teamName'>" + feature.properties.TeamName + "</span>" + "<br />" + "<span class='playerPosition'>" + feature.properties.Position + "</span>" + "<br />" + "<span class='playerHeight'>" + feature.properties.Height + "</span>" + " in" + "<br />" + "<span class='playerWeight'>" + feature.properties.Weight + "</span>" + " lbs" + "<br />" + "<span class='lastSchool'>" + feature.properties.LastSchool + "</span>")
     }
 });
 
@@ -63,3 +63,37 @@ for (let input of document.querySelectorAll('input')) {
 
 /****** INIT ******/
 updateCheckboxStates()
+
+//var clicked = false;
+//
+//$("input[type=checkbox]").click(function () {
+//    if (clicked == false) {
+//        $(this).parent("label").parent("div").css("background-color", "rgba(107, 107, 107)");
+//        $(this).parent("label").parent("div").css("border-color", "rgba(107, 107, 107)");
+//        $(this).parent("label").parent("div").css("color", "#000000");
+//        return clicked = true;
+//    } else {
+//        $(this).parent("label").parent("div").css("background-color", "#000000");
+//        $(this).parent("label").parent("div").css("color", "rgba(107, 107, 107)");
+//        return clicked = false;
+//    }
+//});
+
+//var clicked = false;
+//
+//$("input[type=checkbox]").click(function () {
+//    if (clicked == false) {
+//        $(this).sibling("span").css("color", "rgba(107, 107, 107, 1)");
+//        clicked = true;
+//    } else {
+//        $(this).sibling("span").css("color", "rgba(255, 237, 77, 1)");
+//        clicked = false;
+//    }
+//})
+
+$("input[type=checkbox]").change(function () {
+    var textChange = this.checked ? 'rgba(0, 0, 0, 1' : 'rgba(107, 107, 107, 1)';
+    var backgroundChange = this.checked ? 'rgba(107, 107, 107, 1)' : 'rgba(0, 0, 0, 1)';
+    $(this).parent("label").css('color', textChange);
+    $(this).parent("label").parent("div").css('background-color', backgroundChange);
+});
